e33ee8563c27668df8a263c71121d9e0d00d464e
Gustav Ryberg Smidt <gurysm@me.com>
2023-10-05 12:22:06 +0200

    Update txt2unicode.py

--- txt2unicode.py
+++ txt2unicode.py
@@ -1,30 +1,23 @@
 import re
 import requests
 
-# read sign list from github
-
 response = requests.get('https://raw.githubusercontent.com/tosaja/Nuolenna/master/sign_list.txt')
 data = response.text
-data_lines = data.split('\n')
-# we start by making a list without transliterations that begin with '...', a slash '/' or a pip '|' and it is called unicode_lines
-unicode_lines = ''
-for sign in range(len(data_lines)-1):
-    if data_lines[sign][0:3] == '...' or data_lines[sign][0] == '/' or data_lines[sign][0] == '|':
-        continue
-    else:
-        unicode_lines += data_lines[sign] + '\n'
+data.replace('ka\t𒅟','ka\t𒅗')
+#location of the where you want to save the downloaded sign list
+with open('/Users/gustavrybergsmidt/Documents/OBabat/sign_list_2.txt','w') as f:
+    f.write(data)
+#location where you saved the sign list
+sign_list = pd.read_csv('/Users/gustavrybergsmidt/Documents/OBabat/sign_list_2.txt', sep='\t', header=None,keep_default_na=False,index_col=0)
+dropped_lines = list(filter(lambda x: (x[0:3] == '...' or x[0] == '/' or x[0] == '|'),sign_list.index))
+sign_list.drop(dropped_lines)
 
-# correct mistake for ka
-unicode_lines = unicode_lines.replace('ka\t𒅟','ka\t𒅗')
-
-# change text file to unicode cunefirom
-# locate first the file you want to change, the assumption here is that it contains lines seperated by newlines and each line has a tab-separated label, if you don't have labels or newlines as separaters you can remove those parts
-txt_file_2_change = ''
+#pre-defined labels based on typical ORACC data as pos-tags
+labels = ['O', 'V', 'MOD', 'AV', 'PN', 'SBJ', 'PRP', 'N', 'nan', 'CNJ', 'NU', 'IP', 'AJ', 'DET', 'DP', 'SN', 'DN', 'REL', 'RN', 'J', 'XP', 'MN', 'QP', 'WN', 'RP', 'GN', 'EN', 'PP', 'TN']
+#location for file that is going to be changed, which is currently set  to be a tab-separated list with transliteration and label
+txt_file_2_change = '/Users/gustavrybergsmidt/Documents/OBabat/NLP data set/EPOS_sentence_sep'
 with open(txt_file_2_change,'r') as f:
     text_data = f.readlines()
-
-# define the list of possible labels
-labels = []
 
 unicode_text = ''
 failed = []
@@ -42,16 +35,17 @@ for line in text_data:
             elif sign.group() == 'x':
                 
                 unicode_text += 'x'
-                
             else:
-                if len(unicode_lines.loc[unicode_lines[0]==sign.group().lower(),1]) >= 1:
-                    unicode_text += unicode_lines.loc[unicode_lines[0]==sign.group().lower(),1].values[0]
+                
+                if len(sign_list.loc[sign_list.index==sign.group().lower(),1]) >= 1:
+                    unicode_text += sign_list.loc[sign_list.index==sign.group().lower(),1].values[0]
                 else:
                     unicode_text += 'CHECK({sign_miss})'.format(sign_miss=sign.group())
                     failed.append('CHECK({sign_miss})'.format(sign_miss=sign.group()))
 
 failed = list(set(failed))
 
-unicode_txt_file_directory = ''
+#location of results
+unicode_txt_file_directory = '/Users/gustavrybergsmidt/Documents/OBabat/NLP data set/EPOS_sentence_sep_unicode'
 with open(unicode_txt_file_directory,'w') as f:
     f.writelines(unicode_text)
